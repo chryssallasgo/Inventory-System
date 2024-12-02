a4ca2410e2c24cca24096f31463e99a4d3d3fc3a
Allasgo Chryssdale <chryssdaleheartallasgo@gmail.com>
2024-12-02 22:08:46 +0800

    Everything

--- INVendoryApp/resources/views/homepage.blade.php
+++ INVendoryApp/resources/views/homepage.blade.php
@@ -2,7 +2,7 @@
     <div class="z-10 flex flex-col items-center justify-center space-y-2 gap-x-6">
         <img src="https://cdn-icons-png.flaticon.com/128/900/900618.png" alt="Logo" class="sm:ml-10">
         <div class="px-10 pt-5 pb-10 mx-1 rounded-md gap-x-5">
-            <h2 class="py-6 text-5xl text-left text-gray-800 dark:text-white sm:ml--10 fade-in-left ">Computer Parts Inventory Management System</h2>
+            <h2 class="py-6 text-5xl text-left text-gray-800 dark:text-white sm:ml--10 fade-in-left ">Sari Sari Store Inventory Management System</h2>
             <h3 class="text-justify text-gray-800 dark:text-white sm:ml-30 fade-in-left animation-delay-200">Lorem ipsum odor amet, consectetuer adipiscing elit. Condimentum vel ad interdum id cursus. Ante consectetur malesuada suscipit curabitur himenaeos massa velit ligula. Lacus sollicitudin senectus eleifend erat mauris; vehicula montes. Justo lobortis augue, eleifend elementum felis duis lorem morbi. Quam morbi maximus tempus aenean ullamcorper tellus libero. Cursus semper malesuada natoque magna cursus imperdiet vitae. Accumsan sagittis arcu natoque ante et eleifend.</h3>
         </div>
         @if (Route::has('login'))
